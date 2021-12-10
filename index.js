5127412fa76e3e18bf3c2284925c2a03caa7c0db
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-10 16:49:02 -0300

    feat:adding a default route

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/database')
-const router = require('./src/routes/product')
+const routes = require('./src/routes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
-app.use(router)
+app.use('/api', routes)
 
 sequelize.sync()
 
-router.use((req, res) => {
-    res.send('404 not found')
+routes.use((req, res) => {
+    res.status(404).send('404 not found')
 })
 
-router.use((error, req, res, next) => {
+routes.use((error, req, res, next) => {
     console.log(error.stack)
     res.status(500).send("Something went wrong with server =/")
 })
