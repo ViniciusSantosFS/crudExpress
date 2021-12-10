45751b33bc0440791d2292906667e28443d687c7
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-10 14:30:00 -0300

    adjust:dialect

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,19 @@
 const { Sequelize } = require('sequelize');
 
 const dialectOptions = process.env.NODE_ENV === 'production' ?  {
+   ssl: {
+       require: true
+   }
+} : {
     ssl: {
-        key: cKey,
-        cert: cCert,
-        ca: cCA
-      }
-} : {}
+        require: false
+    }
+}
 
 const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
+    ssl: process.env.NODE_ENV === 'production' ? true : false,
     dialectOptions,
 })
 
